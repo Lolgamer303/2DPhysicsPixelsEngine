2501fbd0e9d57eb274fda64d60d59f9b5953ff96
Lolgamer303 <nathanchaps@icloud.com>
2024-12-10 20:55:11 +0100

    added content

--- main.py
+++ main.py
@@ -2,38 +2,49 @@ from pygame import *
 from numpy import *
 import render
 from pixels import PixelType, Pixel
-import random
+from time import sleep
 
-init()
-screen = display.set_mode((800, 600))
-display.set_caption("Pixel Physics Engine")
+def main():
+    init()
+    screen = display.set_mode((800, 600))
+    display.set_caption("Pixel Physics Engine")
 
-FPS = 120
-clock = time.Clock()
+    FPS = 120
+    clock = time.Clock()
 
-SPAWN_SIZE = 3
+    SPAWN_SIZE = 0
 
-pixels = {}
+    pixels = {}
 
-def get_mouse_coords():
-    x, y = mouse.get_pos()
-    return x, y
+    def get_mouse_coords():
+        x, y = mouse.get_pos()
+        return x, y
 
-def handle_mouse_press():
-    if mouse.get_pressed()[0]:
-        x, y = get_mouse_coords()
-        pixels[(x, y)] = Pixel(x, y, PixelType.SAND)
+    def handle_mouse_press():
+        if mouse.get_pressed()[0]:
+            x, y = get_mouse_coords()
+            for i in range(-SPAWN_SIZE, SPAWN_SIZE + 1):
+                for j in range(-SPAWN_SIZE, SPAWN_SIZE + 1):
+                    pixels[(x + i, y + j)] = Pixel(x + i, y + j, PixelType.SAND)
+        if mouse.get_pressed()[2]:
+            x, y = get_mouse_coords()
+            for i in range(-SPAWN_SIZE-2, SPAWN_SIZE + 2):
+                for j in range(-SPAWN_SIZE-2, SPAWN_SIZE + 2):
+                    pixels[(x + i, y + j)] = Pixel(x + i, y + j, PixelType.STONE)
 
-running = True
-while running:
+    running = True
+    while running:
 
-    for e in event.get():
-        if e.type == QUIT:
-            running = False
-    frameTime = clock.tick(FPS) / 1000.0
-    handle_mouse_press()
-    screen.fill((0, 0, 0))
-    render.render(screen, pixels=pixels, deltaTime=frameTime)
+        for e in event.get():
+            if e.type == QUIT:
+                running = False
+        frameTime = clock.tick(FPS) / 1000.0
+        handle_mouse_press()
+        screen.fill((0, 0, 0))
+        render.render(screen, pixels=pixels, deltaTime=frameTime)
 
-    display.flip()
-quit()
+        display.flip()
+    quit()
+
+if __name__ == "__main__":
+    main()
--- pixels.py
+++ pixels.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pygame
 import enum
+import math
+import random
 
 class PixelType(enum.Enum):
 
@@ -16,25 +18,50 @@ class PixelType(enum.Enum):
         self.id = id
     
     SAND = (194, 178, 128, 1)
-    AIR = (0, 0, 0, 2)
+    STONE = (100, 100, 100, 2)
 
 class Pixel:
     def __init__(self, x, y, type: PixelType):
         self.x: float = x
         self.y: float = y
         self.type: PixelType = type
-        self.velocity: float = 0
+        self.velocity: float = 0.01
 
-    def draw(self, screen):
-        pygame.draw.rect(screen, (self.type.r, self.type.g, self.type.b), (self.x, self.y, 1, 1))
-    def draw(self, screen, size):
+    def draw(self, screen, size=1):
         pygame.draw.rect(screen, (self.type.r, self.type.g, self.type.b), (self.x, self.y, size, size))
 
     def update(self, deltaTime, pixels):
         if self.type == PixelType.SAND:
             self.velocity += deltaTime * 9.81 * 10
-            self.y += + self.velocity * deltaTime
-            if self.y > 600:
-                self.y = 599
+            self.y += self.velocity * deltaTime
+            y = math.floor(self.y)
+            self.handleSpace(pixels, y)
+
+    def handleSpace(self, pixels, y):
+        if self.velocity > 0: 
+            if y < 0 or y >= 599:
                 self.velocity = 0
+                self.y = 599
                 return
+        hasAPixelDown = (self.x, y + 1) in pixels and (pixels.get((self.x, y + 1)).velocity == 0 or pixels.get((self.x, y + 1)).type == PixelType.STONE)
+        if hasAPixelDown: 
+            hasAPixelDownLeft = (self.x - 1, y + 1) in pixels and pixels.get((self.x - 1, y + 1)).velocity == 0
+            hasAPixelDownRight = (self.x + 1, y + 1) in pixels and pixels.get((self.x + 1, y + 1)).velocity == 0
+            if hasAPixelDown:
+                if not hasAPixelDownLeft and not hasAPixelDownRight:
+                    self.velocity / 2
+                    l = random.choice([-1, 1])
+                    self.x += l
+                    return
+                if not hasAPixelDownRight:
+                    self.velocity /= 2
+                    self.x += 1
+                    return
+                elif not hasAPixelDownLeft:
+                    self.velocity /= 2
+                    self.x -= 1
+                    return
+                else:
+                    self.velocity = 0
+                    self.y = y
+                    return
